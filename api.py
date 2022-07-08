b0d68206db83c2746e616fd8e76d61f573024e94
Arthur <maximillian.arthur@fazzfinancial.com>
2022-07-09 02:57:36 +0700

    add file

--- api.py
+++ api.py
@@ -6,7 +6,7 @@ app = Flask(__name__)
 
 @app.route("/", methods=["GET"])
 def first():
-    result = "Hellow World"
+    result = "Hello World"
     return (result)
 
 @app.route("/version", methods=["GET"])
